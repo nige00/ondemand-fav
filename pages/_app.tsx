34358e32e0f573fa8540ee2414c04f519d1cecbb
nige00 <89304842+nige199@users.noreply.github.com>
2021-08-22 11:40:35 +0000

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import "tailwindcss/tailwind.css";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { GA_TRACKING_ID, pageview } from "../analytics/gtag";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+  useEffect(() => {
+    // GA_TRACKING_ID が設定されていない場合は、処理終了
+    if (!GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
-export default MyApp;
