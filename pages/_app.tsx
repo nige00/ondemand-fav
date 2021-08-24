ec10883e5f427e694140f3ab06f290d8fc89e883
nige00 <89304842+nige199@users.noreply.github.com>
2021-08-24 08:55:25 +0000

    added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,24 @@ import "tailwindcss/tailwind.css";
 import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import nprogress from 'nprogress' // NProgressインポート
+import 'nprogress/nprogress.css' // バーのデフォルトスタイルのインポート
 import { GA_TRACKING_ID, pageview } from "../analytics/gtag";
 
+nprogress.configure({ showSpinner: false, speed: 400, minimum: 0.25 })
+
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+
+  if (process.browser) {
+    // バーの表示開始
+    nprogress.start()
+  }
+
+  useEffect(() => {
+    // バーの表示終了
+    nprogress.done()
+  })
+
   const router = useRouter();
   useEffect(() => {
     // GA_TRACKING_ID が設定されていない場合は、処理終了
